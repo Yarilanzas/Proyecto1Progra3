d23d8b7c0ccf36a8bb6940f094b5fc1bff25cc73
Yariela Lanzas <yariela.lanzas.cubero@est.una.ac.cr>
2026-08-18 00:38:27 -0600

    juganding

---
+++ Proyecto1Progra3/src/main/java/org/presentation/Login.java
@@ -0,0 +1,15 @@
+package org.presentation;
+import javax.swing.*;
+
+public class Login {
+    private JPanel PrincipalPanel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton cambiarButton;
+    private JButton cancelarButton;
+    private JButton ingresarButton;
+
+    public JPanel getPrincipalPanel() {
+        return PrincipalPanel;
+    }
+}
